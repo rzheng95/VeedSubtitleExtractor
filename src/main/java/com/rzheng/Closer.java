d2151d6af8a2722cef0ba68ffd9bd0ea7ba56071
Richard Zheng <ruixiangzheng95@gmail.com>
2022-07-24 15:45:46 -0500

    Initial commit

---
+++ src/main/java/com/rzheng/Closer.java
@@ -0,0 +1,19 @@
+package com.rzheng;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class Closer implements Closeable {
+    private Closeable closeable;
+
+    public <T extends Closeable> T using(T t) {
+        closeable = t;
+        return t;
+    }
+
+    @Override public void close() throws IOException {
+        if (closeable != null) {
+            closeable.close();
+        }
+    }
+}
---
+++ src/main/java/com/rzheng/Main.java
@@ -0,0 +1,99 @@
+package com.rzheng;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        final String SOURCE_FILE_PATH = "source.txt",
+                     OUTPUT_FILE_PATH = "output.txt";
+
+        writeFile(OUTPUT_FILE_PATH, readFile(SOURCE_FILE_PATH));
+    }
+
+    public static String readFile(String fileName) {
+        String fileContent = "";
+        try {
+            File f = new File(fileName);
+            if (f.isFile() && f.exists()) {
+                InputStreamReader read = new InputStreamReader(new FileInputStream(f), "UTF-8");
+                BufferedReader reader = new BufferedReader(read);
+                String line;
+
+                int lineCount = 1;
+                String subtitle = "";
+                String startTime = "";
+                String endTime = "";
+                while ((line = reader.readLine()) != null) {
+                    if (isSubtitle(line)) {
+                        subtitle += extractSubtitleFromLine(line);
+                    }
+
+                    if (isTime(line)) {
+                        String time = extractTimeFromLine(line);
+                        if (startTime.isBlank()) {
+                            startTime = time;
+                        } else {
+                            endTime = time;
+                        }
+                    }
+
+                    /**
+                     * populate fileContent once we have
+                     *  1. Start Time
+                     *  2. End Time
+                     *  3. Current Subtitle
+                     */
+                    if (!subtitle.isBlank() && !startTime.isBlank() && !endTime.isBlank()) {
+                        fileContent += lineCount++ + "\n";
+                        fileContent += "00:" + startTime + "00" + " --> " + "00:" + endTime + "00" + "\n";
+                        fileContent += subtitle + "\n\n";
+
+                        startTime = "";
+                        endTime = "";
+                        subtitle = "";
+                    }
+                }
+                read.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return fileContent;
+    }
+
+    private static boolean isTime(String line) {
+        return line.contains("value=");
+    }
+
+    private static boolean isSubtitle(String line) {
+        return line.contains("<span data-text=\"true\">");
+    }
+
+    private static String extractSubtitleFromLine(String line) {
+        final int START_POS = 112,
+                  END_POS = line.length() - 14;
+        return line.substring(START_POS, END_POS);
+    }
+
+    private static String extractTimeFromLine(String line) {
+        final int START_POS = line.length() - 11,
+                  END_POS = line.length() - 4;
+        return line.substring(START_POS, END_POS).replace(".", ",");
+    }
+
+    public static void writeFile(String fileName, String fileContent) {
+        try {
+            File f = new File(fileName);
+            if (!f.exists()) {
+                f.createNewFile();
+            }
+            OutputStreamWriter write = new OutputStreamWriter(
+                    new FileOutputStream(f), "UTF-8");
+            BufferedWriter writer = new BufferedWriter(write);
+            writer.write(fileContent);
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
